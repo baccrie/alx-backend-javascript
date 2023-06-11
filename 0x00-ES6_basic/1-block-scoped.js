bcf1fc0019b2ccdf8a396f851e69c85b469f4b82
baccrie <bakarerilwan1@gmail.com>
2023-06-11 10:24:11 +0100

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock (trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line
-    const task = true;
-    // eslint-disable-next-line
-    const task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
