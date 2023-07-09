981cd62fd83ada3048c8540d4693e2e777d501b7
baccrie <bakarerilwan1@gmail.com>
2023-07-09 11:24:22 +0100

    Task 11 corrected.

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
   const Employee = {
-    departmentName: [...employees],
   };
+  Employee[departmentName] = [...employees];
 
   return Employee;
 }
