7dcae24588803f9098670216b9113777ed64e947
baccrie <bakarerilwan1@gmail.com>
2023-07-09 11:07:55 +0100

    Task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,12 @@
 export default function appendToEachArrayValue(array, appendString) {
   // eslint-disable-next-line no-unused-vars
-  for (let idx of array) {
-    idx += appendString;
+  const newArray = [];
+  let newV;
+  for (const idx of array) {
+    newV = appendString + idx;
+    newArray.push(newV);
   }
-
+  // eslint-disable-next-line no-param-reassign
+  array = newArray;
   return array;
 }
