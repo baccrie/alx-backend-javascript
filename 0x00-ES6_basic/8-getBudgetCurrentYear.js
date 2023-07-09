84b1fed40d9e28d31ac64699dc86585aba759448
baccrie <bakarerilwan1@gmail.com>
2023-07-09 10:19:00 +0100

    Task 8 new object

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-new-object */
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = new Object();
+
+  budget[`income-${getCurrentYear()}`] = income;
+  budget[`gdp-${getCurrentYear()}`] = gdp;
+  budget[`capita-${getCurrentYear()}`] = capita;
+
+  return budget;
+}
