91fe578d3d56630e052cf4bfd0b50a6f1e5539d4
baccrie <bakarerilwan1@gmail.com>
2023-07-16 11:24:12 +0100

    Task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/extensions
+import ClassRoom from './0-classroom.js';
+
+// eslint-disable-next-line no-unused-vars
+function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(24)];
+}
