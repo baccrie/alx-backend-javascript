5303f95c8a5ac2960ca7c55576958e85529c3a4c
baccrie <bakarerilwan1@gmail.com>
2023-07-23 15:40:32 +0100

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,16 @@
 export default function cleanSet(set, startString) {
-  let res = '';
-  if (!startString || !startString.length) return res;
-  for (const el of set) { if (el && el.startsWith(startString)) res += `${el.slice(startString.length)}-`; }
-  return res.slice(0, res.length - 1);
-}}
+  if (startString === '') {
+    return '';
+  }
+  // eslint-disable-next-line no-unused-vars
+  let retval = '';
+  for (const a of set) {
+    if (a.startsWith(startString)) {
+      // eslint-disable-next-line no-restricted-globals, no-undef
+      retval += `${a.slice((startString).length)}-`;
+    }
+  }
+
+  retval = retval.slice(0, -1);
+  return retval;
+}
