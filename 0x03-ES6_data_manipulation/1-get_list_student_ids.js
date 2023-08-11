7e6baef544996cf650db25f09e6b6fb5147b2990
baccrie <bakarerilwan1@gmail.com>
2023-08-11 17:02:04 +0100

    Task 0

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++
@@ -1,9 +0,0 @@
-export default function getListStudentIds(objects) {
-  // eslint-disable-next-line no-undef, no-unused-vars, valid-typeof
-  if (!Array.isArray(objects)) {
-    return [];
-  }
-  // eslint-disable-next-line no-unused-vars
-  const newObj = objects.map((dict) => dict.id);
-  return newObj;
-}
