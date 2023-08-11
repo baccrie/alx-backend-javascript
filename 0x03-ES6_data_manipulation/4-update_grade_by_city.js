37f026d0d3c4145dbdcdd8cff455a53d5edf9eb6
baccrie <bakarerilwan1@gmail.com>
2023-08-11 18:20:40 +0100

    Task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-param-reassign */
+export default function updateStudentGradeByCity(studentsList, city, gradesList) {
+  const retArray = studentsList.filter((students) => students.location === city).map((elem) => {
+    for (const grad of gradesList) {
+      if (grad.studentId === elem.id) {
+        elem.grade = grad.grade;
+      } else {
+        elem.grade = 'N/A';
+      }
+      return elem;
+    }
+  });
+  return retArray;
+}
