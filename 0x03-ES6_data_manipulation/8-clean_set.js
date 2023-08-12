38e1e066d90655cc8eddc4f07d2de4a9dca186c7
baccrie <bakarerilwan1@gmail.com>
2023-08-12 07:37:52 +0100

    Task 8 new corr4

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,14 @@
-export default function cleanSet(set, str) {
-  if (!str || !str.length) return '';
-  let values = '';
-  for (const i of set) {
-    if (i && i.startsWith(str)) {
-      values += values.length === 0 ? i.replace(str, '') : i.replace(str, '-');
+/* eslint-disable no-else-return */
+export default function cleanSet(sets, start) {
+  let retVal = '';
+  if (start === '' || !start) {
+    return retVal;
+  } else {
+    for (const i of sets) {
+      if (i && i.startsWith(start)) {
+        retVal += retVal.length === 0 ? i.replace(start, '') : i.replace(start, '-');
+      }
     }
   }
-  return values;
+  return retVal;
 }
